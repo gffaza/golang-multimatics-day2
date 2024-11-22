cb0b5a09cd02c984f5ac7311cd2b2f0eccaae8e5
Ghilman Faza <72027610+gffaza@users.noreply.github.com>
2024-11-22 10:44:39 +0700

    add var & const

--- main.go
+++ main.go
@@ -4,4 +4,18 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello BSI Go for Future!")
+
+	//variable -> nilai bisa berubah
+	var name string = "John Doe"
+	country := "Indonesia"
+
+	println(name)
+	println(country)
+
+	//constant -> nilai tidak bisa berubah
+	const pi = 3.14
+	const AppName = "Belajar Golang"
+
+	println(AppName)
+
 }
